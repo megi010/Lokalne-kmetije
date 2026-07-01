d7f9a9fcaa803120aad1c787a016e6fe4e660e38
megi010 <ms01010@student.uni-lj.si>
2026-07-01 17:29:07 +0200

    sprememba csv dat

--- farm_system/database.py
+++ farm_system/database.py
@@ -7,7 +7,7 @@ def get_connection():
     """Vzpostavi povezavo z SQLite bazo."""
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row # Omogoča dostop do podatkov po imenih stolpcev
-    conn.execute("PRAGMA foreign_keys = ON")   
+    conn.execute("PRAGMA foreign_keys = ON")   #preveri veljavnost
     return conn
 
 def query(sql, params=()):
